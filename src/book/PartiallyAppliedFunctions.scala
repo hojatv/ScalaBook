e6d12da20803c3829587fb45c55e3c4cd0727d73
hovaheb <hojat.vaheb@gmail.com>
2016-11-28 11:36:24 +0800

    commit!

--- src/book/PartiallyAppliedFunctions.scala
+++ src/book/PartiallyAppliedFunctions.scala
@@ -6,21 +6,28 @@ import java.util.Date
   * Created by hovaheb on 11/25/2016.
   */
 class PartiallyAppliedFunctions {
-  def log (date: Date , message:String ): Unit ={
+  def log(date: Date, message: String): Unit = {
     println("date: " + date + " and massage: " + message)
   }
 
+  def loopThrough(number: Int, closure: Int => Unit) {
+    for (i <- 1 to number) {
+      closure(i)
+    }
+  }
+
 }
-object PartiallyAppliedFunctions{
+
+object PartiallyAppliedFunctions {
   def main(args: Array[String]) {
-    val p:PartiallyAppliedFunctions = new PartiallyAppliedFunctions
+    val p: PartiallyAppliedFunctions = new PartiallyAppliedFunctions
     p.log(new Date(), "message1")
     p.log(new Date(), "message2")
     p.log(new Date(), "message3")
 
     /*When you create a partially applied function, Scala internally creates a
     new class with a special apply( ) method*/
-    val logWithDateBound = p.log(new Date(),_:String)
+    val logWithDateBound = p.log(new Date(), _: String)
     logWithDateBound("message1")
     logWithDateBound("message2")
     logWithDateBound("message3")
