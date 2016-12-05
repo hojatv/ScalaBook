9f61fd19d4f93b9853c02534017cf36b6646ce28
hovaheb <hojat.vaheb@gmail.com>
2016-12-05 14:36:30 +0800

    commit!

---
+++ src/main/java/book/Unary.scala
@@ -0,0 +1,25 @@
+package book
+
+
+/**
+  * Created by hovaheb on 11/29/2016.
+  */
+class Unary {
+  class Sample {
+    def unary_+ = println("Called unary +" )
+    def unary_- = println("called unary -" )
+    def unary_! = println("called unary !" )
+    def unary_~ = println("called unary ~" )
+  }
+
+}
+object Unary{
+  def main(args: Array[String]): Unit = {
+    val u:book.Unary = new book.Unary
+    val sample = new u.Sample
+    +sample
+    -sample
+    !sample
+    ~sample
+  }
+}
---
+++ src/main/java/book/intermixingwithjava/investments/InvestmentType.java
@@ -0,0 +1,14 @@
+package book.intermixingwithjava.investments;
+
+/**
+ * Created by hovaheb on 12/5/2016.
+ */
+public enum InvestmentType {
+    SHORT_TERM,
+    BOND,
+    STOCK,
+    REAL_ESTATE,
+    COMMODITIES,
+    COLLECTIBLES,
+    MUTUAL_FUNDS
+}
