a59bd9622ba61d313fc597a942e1392fa589dc87
hovaheb <hojat.vaheb@gmail.com>
2016-12-23 14:31:08 +0800

    commit!

---
+++ src/main/java/book/usingscala/NetAssetAppGUI.scala
@@ -0,0 +1,78 @@
+package book.usingscala
+
+import java.awt.Color
+
+import scala.actors._
+import scala.actors.scheduler.SingleThreadedScheduler
+import scala.swing._
+import scala.swing.event.ButtonClicked
+
+/**
+  * Created by hovaheb on 12/23/2016.
+  */
+object NetAssetAppGUI extends SimpleSwingApplication {
+  override def top = mainFrame
+
+  val mainFrame = new MainFrame {
+    title = "Net Asset"
+    val dateLabel = new Label {
+      text = "Last Updated : -----"
+    }
+    val valuesTable = new Table(NetAssetStockPriceHelper.getInitialTableValues, Array("Ticker", "Units", "Price", "Value")) {
+      showGrid = true
+      gridColor = Color.BLACK
+    }
+    val updateButton = new Button {
+      text = "update"
+    }
+    val netAssetLabel = new Label {
+      text = "Net Asset: ????"
+    }
+    contents = new BoxPanel(Orientation.Vertical) {
+      contents += dateLabel
+      contents += valuesTable
+      contents += new ScrollPane(valuesTable)
+      contents += new FlowPanel {
+        contents += updateButton
+        contents += netAssetLabel
+      }
+    }
+    listenTo(updateButton)
+    reactions += {
+      case ButtonClicked(button) =>
+        button.enabled = false
+        NetAssetStockPriceHelper fetchPrice uiUpdater
+    }
+
+    val uiUpdater = new Actor {
+      override def act(): Unit = {
+        loop {
+          react {
+            case (symbol: String, units: Int, price: Double, value: Double) =>
+              updateTable(symbol, units, price, value)
+            case netAsset =>
+              netAssetLabel.text = "NetAsset: " + netAsset
+              dateLabel.text = "Last Updated: " + new java.util.Date()
+              updateButton.enabled = true
+
+
+          }
+        }
+      }
+
+      override  def scheduler() = new SingleThreadedScheduler
+    }
+    uiUpdater.start()
+
+    def updateTable(symbol: String, units: Int, price: Double, value: Double): Unit = {
+      for (i <- 0 until valuesTable.rowCount) {
+        if (valuesTable(i, 0) == symbol) {
+          valuesTable(i, 2) = price
+          valuesTable(i, 3) = value
+        }
+      }
+    }
+  }
+}
+
+
--- src/main/java/book/usingscala/NetAssetStockPriceHelper.scala
+++ src/main/java/book/usingscala/NetAssetStockPriceHelper.scala
@@ -1,13 +1,48 @@
 package book.usingscala
+
 /**
   * Created by hovaheb on 12/22/2016.
   */
+
+import scala.actors._
+import Actor._
+
 object NetAssetStockPriceHelper {
   val symbolsAndUnits = StockPriceFinder.getTickersAndUnits()
 
-  def initialTableValues : Array[Array[Any]] = {
-    //val emptyArrayOfArrayOfAny = new Array[Array[Any]](0,0)
-    //emptyArrayOfArrayOfAny
-    null
+  def getInitialTableValues: Array[Array[Any]] = {
+    val emptyArrayOfArrayOfAny = new Array[Array[Any]](0)
+    (emptyArrayOfArrayOfAny /: symbolsAndUnits) { (data, element) =>
+      val (symbol, units) = element
+      data ++ Array(List(symbol, units, "?", "?").toArray)
+    }
+
+  }
+
+  def fetchPrice(updater: Actor) = actor {
+    val caller = self
+    symbolsAndUnits.keys.foreach { symbol =>
+      actor {
+        caller ! (symbol, StockPriceFinder.getLatestClosingPrice(symbol))
+      }
+    }
+
+    val netWorth = (0.0 /: (1 to symbolsAndUnits.size)) { (worth, index) =>
+      receive {
+        case (symbol: String, latestClosingPrice: Double) =>
+          val units = symbolsAndUnits(symbol)
+          val value = units * latestClosingPrice
+          updater ! (symbol, units, latestClosingPrice, value)
+          worth + value
+      }
+    }
+    updater ! netWorth
+  }
+
+  def main(args: Array[String]): Unit = {
+    val result = getInitialTableValues
+    for (i <- result) {
+      println(i.mkString)
+    }
   }
 }
