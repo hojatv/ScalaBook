7be6cc9e67bb7f1a7f1f5ca1c2728bc62b124e86
hovaheb <hojat.vaheb@gmail.com>
2016-12-29 16:56:29 +0800

    commit!

---
+++ src/main/java/book/cuncurrentprogramming/akka/GreetingsActor.scala
@@ -0,0 +1,22 @@
+package book.cuncurrentprogramming.akka
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+/**
+  * Created by hovaheb on 12/29/2016.
+  */
+case class Name(name:String)
+class GreetingsActor extends Actor{
+  override def receive: Receive = {
+    case Name(n)=> println("Hello " + n)
+  }
+}
+object GreetingsActor{
+  def main(args: Array[String]): Unit = {
+    val system = ActorSystem("greetings")
+    val a = system.actorOf(Props[GreetingsActor], name = "greeting-actor")
+    a ! "Reza"
+    system.shutdown()
+
+  }
+}
