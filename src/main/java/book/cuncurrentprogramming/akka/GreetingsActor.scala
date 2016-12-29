51ca187a2764ceaa5f9ca769ae20a08906c6d206
hojatv <hojat.vaheb@gmail.com>
2016-12-29 18:06:16 +0800

    from Home!

--- src/main/java/book/cuncurrentprogramming/akka/GreetingsActor.scala
+++ src/main/java/book/cuncurrentprogramming/akka/GreetingsActor.scala
@@ -1,6 +1,6 @@
 package book.cuncurrentprogramming.akka
 
-import akka.actor.{Actor, ActorSystem, Props}
+import akka.actor.{Actor, ActorPath, ActorRef, ActorSystem, Props}
 
 /**
   * Created by hovaheb on 12/29/2016.
@@ -13,9 +13,25 @@ class GreetingsActor extends Actor{
 }
 object GreetingsActor{
   def main(args: Array[String]): Unit = {
+    /*The actor system will create the infrastructure required for the actor to run.*/
     val system = ActorSystem("greetings")
     val a = system.actorOf(Props[GreetingsActor], name = "greetings-actor")
     a ! Name("Reza")
+
+    /*The system / method returns the actor path*/
+    val path: ActorPath = system / "greetings-actor"
+    println(path)
+    val actorRef: ActorRef = system.actorFor(path)
+    println(actorRef)
+    actorRef ! Name("some message")
+
+    val deadLetterActorRef: ActorRef = system.actorFor(path + "alaki")
+    println(deadLetterActorRef)
+    /*If the actorFor fails to find an actor
+    pointed to by the path, it returns a reference to the dead-letter mailbox of the actor    system.*/
+    deadLetterActorRef ! Name("some message")
+
+
     system.shutdown()
 
   }
